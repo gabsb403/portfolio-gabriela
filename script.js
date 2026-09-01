48aa4aa6e9d19fb30a853a3d3b70c2484d71b1e7
gabsb403 <gabrielacontasnub@gmail.com>
2026-08-31 21:40:17 -0300

    feat: adiciona funcionalidade localStorage para tema claro/escuro.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const temaSalvo = localStorage.getItem('tema');
+ if (temaSalvo === 'light') {
+  document.documentElement.setAttribute('data-theme', temaSalvo);
+ }
+
 document.getElementById('theme-toggle').addEventListener('click', function() {
   const botao = document.getElementById('theme-toggle');
   
   if (document.documentElement.getAttribute('data-theme') === 'light') {
     document.documentElement.removeAttribute('data-theme');
     botao.textContent = '🌙';
+     localStorage.setItem('tema', 'dark')
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     botao.textContent = '☀️';
+    localStorage.setItem('tema', 'light')
   }
 });
